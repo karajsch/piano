ed6d51afd4a3b85cd22dd526cc790aea4e1c934d
Kara Schmidt <kara.jane.schmidt@gmail.com>
2021-06-26 15:48:26 -0400

    changed pandas method from concat to merge because that makes more sense, also cleaned up blank columns in CSV's

--- python.py
+++ python.py
@@ -2,10 +2,16 @@
 import pandas as pd
 
 # Importing 2 files
-files = ["FileA.csv", "FileB.csv"]
+#files = ["FileA.csv", "FileB.csv"]
+
+data1 = pd.read_csv('FileA.csv')
+data2 = pd.read_csv('FileB.csv')
 
 # Using concat method to combine both files
-file_c = pd.concat([pd.read_csv(f) for f in files])
+file_c = pd.merge(data1, data2, 
+    on='user_id', how='inner')
+    
+#([pd.read_csv(f) for f in files])
 
 # This will export the new file as a CSV
 file_c.to_csv("FileC.csv", index=False, encoding='utf-8-sig')
