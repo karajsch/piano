aa01824c88c0dfc1571249e5b2e67cd7d3c11183
Kara Schmidt <kara.jane.schmidt@gmail.com>
2021-06-27 15:47:01 -0400

    adding API user request section with authentication

--- piano-user-update.py
+++ piano-user-update.py
@@ -13,3 +13,21 @@ mergedData = pd.merge(data1, data2,
 
 # Print merged data for testing
 print(mergedData)
+
+
+### API Request Section ###
+my_headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Accept': 'application/json'
+}
+
+my_params = {
+    'aid': 'o1sRRZSLlw',
+    'api_token': 'zziNT81wShznajW2BD5eLA4VCkmNJ88Guye7Sw4D'
+}
+
+response = requests.post(
+    'https://sandbox.tinypass.com/api/v3/publisher/user/search', headers=my_headers, params=my_params)
+
+# Print for testing
+print(response.text)
